4fa4b9d7d7373afef2a1699490981c95c014c63d
Krusheel <krusheelkumar7@gmail.com>
2026-09-21 12:22:18 +0530

    Fix: Enable Spring Scheduling for cron jobs

--- src/main/java/com/popobob/giftcard/GiftcardBackendApplication.java
+++ src/main/java/com/popobob/giftcard/GiftcardBackendApplication.java
@@ -2,8 +2,10 @@ package com.popobob.giftcard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class GiftcardBackendApplication {
 
 	public static void main(String[] args) {
