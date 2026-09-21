bb50d2e9b6b82b18705755a3aa6ff48ae7a78a5f
Krusheel <krusheelkumar7@gmail.com>
2026-09-21 13:25:07 +0530

    Force JVM, Jackson, and Hibernate timezone to Asia/Kolkata (IST)

--- src/main/java/com/popobob/giftcard/GiftcardBackendApplication.java
+++ src/main/java/com/popobob/giftcard/GiftcardBackendApplication.java
@@ -3,10 +3,18 @@ package com.popobob.giftcard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
 public class GiftcardBackendApplication {
+
+    @PostConstruct
+    public void init() {
+        // Force the entire application (and database timestamps) to use Indian Standard Time
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(GiftcardBackendApplication.class, args);
